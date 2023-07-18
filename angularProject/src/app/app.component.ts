fad6cf6a38f0610e792292f8d87da3d0a15403f6
ismael <ismaelreta.programacion@gmail.com>
2023-07-18 14:26:01 -0300

    a list of stuff

--- angularProject/src/app/app.component.ts
+++ angularProject/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+
+interface Product {
+  name: string;
+  cost: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'angularProject';
+  products: Product[] = [
+    { name: 'Roasted porg', cost: '11'},
+    { name: 'Meiloorun fruit', cost: '38'},
+    { name: 'Photon fizzle', cost: '15'},
+    { name: 'Dex\'s double', cost: '30'},
+  ];
 }
